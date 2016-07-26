63a051e395c3ad4e4c1dc40fce03a06c58d4ebb0
Benoist LAURENT <benoist.laurent@gmail.com>
2016-07-26 15:17:37 +0200

    Movement bindings: implemented inheritance

--- bindings/movement.pyx
+++ bindings/movement.pyx
@@ -9,32 +9,6 @@ cdef extern from "Movement.h" namespace "PTools":
         void Apply(CppRigidbody&)
 
 
-    cdef cppclass CppShift "PTools::Movement":
-        CppShift()
-
-    cdef cppclass CppSlide "PTools::Movement":
-        CppSlide()
-
-    cdef cppclass CppRise "PTools::Movement":
-        CppRise()
-
-    cdef cppclass CppTwist "PTools::Movement":
-        CppTwist()
-
-    cdef cppclass CppRoll "PTools::Movement":
-        CppRoll()
-
-    cdef cppclass CppTilt "PTools::Movement":
-        CppTilt()
-
-    cdef cppclass CppADNA "PTools::Movement":
-        CppADNA()
-
-    cdef cppclass CppBDNA "PTools::Movement":
-        CppBDNA()
-
-
-
 cdef class Movement:
     cdef CppMovement *thisptr
     def __cinit__(self):
@@ -46,89 +20,26 @@ cdef class Movement:
         self.thisptr.Apply(deref(rigid.thisptr))
 
 
-cdef class Shift:
-    cdef CppShift *thisptr
-    
-    def __cinit__(self):
-        self.thisptr = new CppShift()
-    
-    def __dealloc__(self):
-        if self.thisptr:
-            del self.thisptr
+cdef class Shift(Movement):
+    pass
 
+cdef class Slide(Movement):
+    pass
 
-cdef class Slide:
-    cdef CppSlide *thisptr
-    
-    def __cinit__(self):
-        self.thisptr = new CppSlide()
-    
-    def __dealloc__(self):
-        if self.thisptr:
-            del self.thisptr
+cdef class Rise(Movement):
+    pass
 
+cdef class Twist(Movement):
+    pass
 
-cdef class Rise:
-    cdef CppRise *thisptr
-    
-    def __cinit__(self):
-        self.thisptr = new CppRise()
-    
-    def __dealloc__(self):
-        if self.thisptr:
-            del self.thisptr
+cdef class Roll(Movement):
+    pass
 
+cdef class Tilt(Movement):
+    pass
 
-cdef class Twist:
-    cdef CppTwist *thisptr
-    
-    def __cinit__(self):
-        self.thisptr = new CppTwist()
-    
-    def __dealloc__(self):
-        if self.thisptr:
-            del self.thisptr
+cdef class ADNA(Movement):
+    pass
 
-
-cdef class Roll:
-    cdef CppRoll *thisptr
-    
-    def __cinit__(self):
-        self.thisptr = new CppRoll()
-    
-    def __dealloc__(self):
-        if self.thisptr:
-            del self.thisptr
-
-
-cdef class Tilt:
-    cdef CppTilt *thisptr
-    
-    def __cinit__(self):
-        self.thisptr = new CppTilt()
-    
-    def __dealloc__(self):
-        if self.thisptr:
-            del self.thisptr
-
-cdef class ADNA:
-    cdef CppADNA *thisptr
-    
-    def __cinit__(self):
-        self.thisptr = new CppADNA()
-    
-    def __dealloc__(self):
-        if self.thisptr:
-            del self.thisptr
-
-
-cdef class BDNA:
-    cdef CppBDNA *thisptr
-    
-    def __cinit__(self):
-        self.thisptr = new CppBDNA()
-    
-    def __dealloc__(self):
-        if self.thisptr:
-            del self.thisptr
-
+cdef class BDNA(Movement):
+    pass
