9873c4e98a2f9c075e385a5a4afb9362c3a6089c
Benoist LAURENT <benoist.laurent@gmail.com>
2016-09-30 15:34:47 +0200

    tests: moved superpose tests to test_superpose.py

---
+++ Tests/test_superpose.py
@@ -0,0 +1,40 @@
+
+import os
+import random
+import unittest
+
+from ptools import Rigidbody, Coord3D, superpose, Rmsd
+
+
+TEST_1FINR_PDB = os.path.join(os.path.dirname(__file__), 'data', '1FIN_r.pdb')
+
+
+
+class TestSuperposition(unittest.TestCase):
+    def setUp(self):
+        self.prot1 = Rigidbody(TEST_1FINR_PDB)
+        random.seed(123)
+    def testTransRot(self):
+        prot2 = Rigidbody(self.prot1)
+
+
+        for i in xrange(20):
+            #random translation coordinates:
+            x = (random.random()-0.5)*50.0
+            y = (random.random()-0.5)*50.0
+            z = (random.random()-0.5)*50.0
+            prot2.Translate(Coord3D(x,y,z))
+            a = (random.random()-0.5)*50.0
+            b = (random.random()-0.5)*50.0
+            c = (random.random()-0.5)*50.0
+            prot2.AttractEulerRotate(a,b,c)
+
+            sup = superpose(self.prot1,prot2) # superpose(reference, mobile)
+            matrix = sup.matrix
+            prot2.ApplyMatrix(matrix)
+            self.assertTrue(Rmsd(prot2,self.prot1)<1e-6)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- Tests/unittest1.py
+++ Tests/unittest1.py
@@ -178,34 +178,7 @@ class TestRotations(unittest.TestCase):
         self.assertAlmostEqual(co4.z, 1)
         
         
-        
 
-
-
-
-class TestSuperposition(unittest.TestCase):
-    def setUp(self):
-        self.prot1 = Rigidbody(TEST_1FINR_PDB)
-        random.seed(123)
-    def testTransRot(self):
-        prot2 = Rigidbody(self.prot1)
-
-
-        for i in xrange(20):
-            #random translation coordinates:
-            x = (random.random()-0.5)*50.0
-            y = (random.random()-0.5)*50.0
-            z = (random.random()-0.5)*50.0
-            prot2.Translate(Coord3D(x,y,z))
-            a = (random.random()-0.5)*50.0
-            b = (random.random()-0.5)*50.0
-            c = (random.random()-0.5)*50.0
-            prot2.AttractEulerRotate(a,b,c)
-
-            sup = superpose(self.prot1,prot2) # superpose(reference, mobile)
-            matrix = sup.matrix
-            prot2.ApplyMatrix(matrix)
-            self.assertTrue(Rmsd(prot2,self.prot1)<1e-6)
 
 class TestAttractForceField2(unittest.TestCase):
     """ test if calculated energies are stable through library versions """
