5335399398da0290c0e44db9f85a6e816536b888
Benoist LAURENT <benoist.laurent@gmail.com>
2016-10-07 11:26:18 +0200

    tests: assertCoordsAlmostEqual now test x, y and z values instead of distance between two vectors

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -4,7 +4,7 @@ import sys
 from ptools import norm2
 
 
-def assertCoordsAlmostEqual(testcase, source, target):
+def assertCoordsAlmostEqual(testcase, source, target, places=6):
     """Assert that two `ptools.Coord3D` instances are almost equal.
 
     Args:
@@ -12,5 +12,9 @@ def assertCoordsAlmostEqual(testcase, source, target):
             `testcase.assertAlmostEqual` method
         source (ptools.Coord3D): tested coordinates
         target (ptools.Coord3D): reference coordinates
+        places (int): equivalent to unittest.TestCase.assertAlmostEqual
+            places argument (see original documentation)
     """
-    testcase.assertAlmostEqual(norm2(source - target), 0.0)
+    testcase.assertAlmostEqual(source.x, target.x, places=places)
+    testcase.assertAlmostEqual(source.y, target.y, places=places)
+    testcase.assertAlmostEqual(source.z, target.z, places=places)
