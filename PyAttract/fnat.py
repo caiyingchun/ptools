6d7f65190e42ef7e99358fc295e877198db0a6af
Benoist LAURENT <benoist.laurent@gmail.com>
2016-10-11 11:30:14 +0200

    PyAttract/fnat.py: prettification

--- PyAttract/fnat.py
+++ PyAttract/fnat.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-from ptools import *
 import sys
 
+from ptools import AttractPairList, AttractRigidbody, Rigidbody
+
+
 def contact(receptor, ligand, cutoff=7):
-    "return residues in interaction, use ptools::pairlist"
-    
+    """return residues in interaction, use ptools::pairlist"""
+
     if isinstance(receptor, Rigidbody):
         receptor = AttractRigidbody(receptor)
-    
+
     if isinstance(ligand, Rigidbody):
         ligand = AttractRigidbody(ligand)
-    
+
     resnblig = []
     for i in range(len(ligand)):
         at = ligand.CopyAtom(i)
@@ -20,31 +22,30 @@ def contact(receptor, ligand, cutoff=7):
     for j in range(len(receptor)):
         at = receptor.CopyAtom(j)
         resnbrec.append(at.residId)
-    
-    
-    pl = AttractPairList(receptor,ligand, cutoff)
-    contactnat = set() # residue list in interaction
+
+    pl = AttractPairList(receptor, ligand, cutoff)
+    contactnat = set()  # residue list in interaction
 
     for i in range(len(pl)):
         ap = pl[i]
         contactnat.add((resnbrec[ap.atrec], resnblig[ap.atlig]))
-    
+
     return contactnat
 
+
 def fnat(receptor, ligcrist, ligprobe):
-    "return native fraction (fnat)"
-    resid= {}  # residue number of the ith atom
-    corig = contact(receptor,ligcrist)
-    cnew = contact(receptor,ligprobe)
-    intersect  =  corig & cnew 
-    f = float(len(intersect))/float(len(corig))
+    """return native fraction (fnat)"""
+    corig = contact(receptor, ligcrist)
+    cnew = contact(receptor, ligprobe)
+    intersect = corig & cnew
+    f = float(len(intersect)) / float(len(corig))
     return f
 
-def main():
 
-    if len(sys.argv) < 4 : 
+def main():
+    if len(sys.argv) < 4:
         print "usage:  fnat.py receptor lig_ref lig"
-	sys.exit(1)
+        sys.exit(1)
     recname = sys.argv[1]
     ligname = sys.argv[2]
     ligname2 = sys.argv[3]
@@ -52,10 +53,9 @@ def main():
     lig2 = AttractRigidbody(ligname2)
     rec = AttractRigidbody(recname)
 
-    FNAT=fnat(rec,lig,lig2)
+    FNAT = fnat(rec, lig, lig2)
     print FNAT
+
 
 if __name__ == "__main__":
     main()
-
-
