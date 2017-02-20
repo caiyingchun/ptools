92e4f1d513819cb8a28b87bcd5e95d04d96a54e5
Benoist LAURENT <benoist.laurent@gmail.com>
2017-02-20 10:39:03 +0100

    refactoring: moved attract ouput reading classes to ptools.io

--- Tests/functionnal/test_attract.py
+++ Tests/functionnal/test_attract.py
@@ -7,12 +7,11 @@ from __future__ import print_function
 
 import filecmp
 import os
-import re
 import shutil
-import StringIO
 
 import pytest
 
+import ptools
 from ptools.scripts import ptools_cli
 
 from .. import skip_on_osx
@@ -33,236 +32,21 @@ from .. import (TEST_SINGLEMINIM_LIGAND,
                 TEST_DOCKING_ATTRACT_OUT)
 
 
-class AttractOutputComparisonError(Exception):
-    pass
+def run_attract_test(args, capfd, ref_output_file):
+    """Run attract with `args`, read the standard output using `capfd` and
+    compare it with the reference output stored in `ref_output_file`."""
+    cmd_args = ptools_cli.parse_command_line(args)
+    cmd_args.func(cmd_args)
 
+    out, err = capfd.readouterr()
+    output_test = ptools.io.parse_attract_output(out)
+    output_ref = ptools.io.read_attract_output(ref_output_file)
 
-class AttractOutputRotation(object):
-    """Class that help storing a rotation data from Attract output string."""
-
-    # Parse a minimization result line.
-    minim_re = re.compile(r'{{ minimization nb (?P<id>\d+) of (?P<total>\d+) ; '
-                          r'cutoff= (?P<cutoff>\d+\.\d+) \(A\) ; '
-                          r'maxiter= (?P<maxiter>\d+)')
-
-    def __init__(self, i=0, minimlist=[], matrix=[], energie=0.0, rmsd=0.0):
-        self.id = i
-        self.minimlist = minimlist
-        self.matrix = matrix
-        self.energie = energie
-        self.rmsd = rmsd
-
-    def number_of_minimizations(self):
-        """Return the number of minimizations ran."""
-        return len(self.minimlist)
-
-    def format_matrix(self):
-        """Return a string reprenting the matrix."""
-        s = '\n'.join(' '.join('{: 14.7f}'.format(value) for value in row)
-                      for row in self.matrix)
-        return s
-
-    @classmethod
-    def from_string(cls, content):
-        """Initialization from string."""
-        def parse_rotation_minim_output(match):
-            typemap = {'id': int, 'total': int, 'cutoff': float, 'maxiter': int}
-            minim = match.groupdict()
-            for key, value in minim.iteritems():
-                minim[key] = typemap[key](value)
-            return minim
-
-        def parse_rotation_data_line(line):
-            tokens = line.split()[1:]
-            rmsd = float('nan') if tokens[3] == 'XXXX' else float(tokens[3])
-            d = dict(trans=int(tokens[0]), rot=int(tokens[1]),
-                     ener=float(tokens[2]), rmsd=rmsd)
-            return d
-
-        minimlist = []
-        matrix = []
-        lines = content.splitlines()
-        i = int(lines[0].split()[-2])
-        for line in lines:
-            match = cls.minim_re.match(line)
-            if match:
-                minimlist.append(parse_rotation_minim_output(match))
-            elif line.startswith('=='):
-                data = parse_rotation_data_line(line)
-            elif line.startswith('MAT'):
-                matrix.append(list(float(tok) for tok in line.split()[1:]))
-        return cls(i, minimlist, matrix, data['ener'], data['rmsd'])
-
-    def cmp(self, other):
-        """Compare two AttractOutputRotation instances.
-
-        Raises:
-            AttractOutputComparisonError: if two rotations are different.
-        """
-        def isnan(num):
-            return num != num
-
-        # Compare identifiers.
-        if self.id != other.id:
-            err = 'identifiers differ: {} != {}'.format(self.id, other.id)
-            raise AttractOutputComparisonError(err)
-
-        # Compare matrices.
-        if self.matrix != other.matrix:
-            err = 'matrices differ:\n{}\n-----\n{}'.format(self.format_matrix(),
-                                                           other.format_matrix())
-            raise AttractOutputComparisonError(err)
-
-        # Compare minimizations.
-        if self.number_of_minimizations() != other.number_of_minimizations():
-            err = 'different number of minimizations: {} != {}'
-            err = err.format(self.number_of_minimizations(),
-                             other.number_of_minimizations())
-            raise AttractOutputComparisonError(err)
-
-        for minim1, minim2 in zip(self.minimlist, other.minimlist):
-            if minim1['cutoff'] != minim2['cutoff']:
-                err = 'minimization {}: cutoffs differ: {} != {}'
-                err = err.format(minim1['id'], minim1['cutoff'], minim2['cutoff'])
-                raise AttractOutputComparisonError(err)
-            if minim1['maxiter'] != minim2['maxiter']:
-                err = 'minimization {}: maxiters differ: {} != {}'
-                err = err.format(minim1['id'], minim1['maxiter'], minim2['maxiter'])
-                raise AttractOutputComparisonError(err)
-
-        # Compare energies.
-        if self.energie != other.energie:
-            err = 'energies differ: {} != {}'.format(self.energie, other.energie)
-            raise AttractOutputComparisonError(err)
-
-        # Compare RMSDs.
-        if self.rmsd != other.rmsd and not isnan(self.rmsd) and isnan(other.rmsd):
-            err = 'RMSDs differ: {} != {}'.format(self.rmsd, other.rmsd)
-            raise AttractOutputComparisonError(err)
-
-
-class AttractOutputTranslation(object):
-    def __init__(self, id=0, rotations=[]):
-        self.id = id
-        self.rotations = rotations
-
-    @classmethod
-    def from_string(cls, content):
-        """Initialization from string."""
-        rotations = []
-        fileobj = StringIO.StringIO(content)
-        transid = int(next(fileobj).split()[-2])
-        for lines in itercontent(fileobj, '----- Rotation nb'):
-            rot = AttractOutputRotation.from_string(lines)
-            rotations.append(rot)
-        return cls(transid, rotations)
-
-    def number_of_rotations(self):
-        """Return the number of rotations."""
-        return len(self.rotations)
-
-    def cmp(self, other):
-        """Compare two AttractOutputTranslation instances.
-
-        Raises:
-            AttractOutputComparisonError: if two translations are different.
-        """
-        if self.id != other.id:
-            err = 'translation {}: identifiers differ: {} != {}'
-            err = err.format(self.id, self.id, other.id)
-            raise AttractOutputComparisonError(err)
-
-        if self.number_of_rotations() != other.number_of_rotations():
-            err = 'translation {}: number of rotations differ: {} != {}'
-            err = err.format(self.id,
-                             self.number_of_translations(),
-                             other.number_of_translations())
-            raise AttractOutputComparisonError(err)
-
-        for rot1, rot2 in zip(self.rotations, other.rotations):
-            try:
-                rot1.cmp(rot2)
-            except Exception as e:
-                err = 'translation {}: rotation {}: {}'.format(self.id, rot1.id, e)
-                raise AttractOutputComparisonError(err)
-
-
-class AttractOutput(object):
-    def __init__(self):
-        self.translations = []
-
-    def number_of_translations(self):
-        """Return the number of translations."""
-        return len(self.translations)
-
-    def number_of_rotations(self):
-        """Return the list of the number of rotations for each translation."""
-        return [t.number_of_rotations() for t in self.translations]
-
-    @classmethod
-    def from_file(cls, path):
-        """Initialization from file."""
-        with open(path, 'rt') as stream:
-            return cls.from_stream(stream)
-
-    @classmethod
-    def from_string(cls, content):
-        """Initialization from string."""
-        return cls.from_stream(StringIO.StringIO(content))
-
-    @classmethod
-    def from_stream(cls, stream):
-        """Initialization from stream (typically file object)."""
-        output = cls()
-        for trans in itercontent(stream, '@@@@@@@ Translation nb '):
-            t = AttractOutputTranslation.from_string(trans)
-            output.translations.append(t)
-        return output
-
-    def cmp(self, other):
-        """Compare two AttractOutput instances.
-
-        Raises:
-            AttractOutputComparisonError: if two outputs are different.
-        """
-        if self.number_of_translations() != other.number_of_translations():
-            err = 'number of translations differ: {} != {}'
-            err = err.format(self.number_of_translations(),
-                             other.number_of_translations())
-            raise AttractOutputComparisonError(err)
-
-        errors = []
-        for trans1, trans2 in zip(self.translations, other.translations):
-            try:
-                trans1.cmp(trans2)
-            except Exception as e:
-                errors.append(e)
-        return errors
-
-
-def itercontent(fileobj, delim):
-    """Iterate over a file content.
-
-    Return the lines between two occurences of `delim`.
-
-    Args:
-        fileobj (file): iterator that allow to iterate over a file lines.
-        delim (str): separator string.
-
-    Returns:
-        iterator[str]: lines between 2 occurences of `delim`.
-    """
-    current_token = ''
-    istoken = False
-    for line in fileobj:
-        if line.startswith(delim):
-            istoken = True
-            if current_token:
-                yield current_token
-                current_token = ''
-        if istoken:
-            current_token += line
-    yield current_token
+    errors = output_ref.assert_equal(output_test)
+    if errors:
+        for e in errors:
+            print(e.message)
+    assert len(errors) == 0
 
 
 @pytest.fixture
@@ -285,18 +69,7 @@ def test_single_minimization(capfd, minim_traj):
             '-c', TEST_SINGLEMINIM_ATTRACT_INP,
             '-p', TEST_SINGLEMINIM_FF_PARAM,
             '-s']
-    cmd_args = ptools_cli.parse_command_line(args)
-    cmd_args.func(cmd_args)
-
-    out, err = capfd.readouterr()
-    output_ref = AttractOutput.from_string(out)
-    output_test = AttractOutput.from_file(TEST_SINGLEMINIM_ATTRACT_OUT)
-
-    errors = output_ref.cmp(output_test)
-    if errors:
-        for e in errors:
-            print(e.message)
-    assert len(errors) == 0
+    run_attract_test(args, capfd, TEST_SINGLEMINIM_ATTRACT_OUT)
     assert filecmp.cmp(TEST_SINGLEMINIM_MINIMIZATION_OUT, output_name) is True
 
 
@@ -318,19 +91,7 @@ def test_docking(capfd, docking_trans_rot):
     args = ['attract',
             '-r', TEST_DOCKING_RECEPTOR,
             '-l', TEST_DOCKING_LIGAND,
-            # '--ref', TEST_DOCKING_LIGAND,
             '-c', TEST_DOCKING_ATTRACT_INP,
             '-p', TEST_DOCKING_FF_PARAM,
             '-t', '3']
-    cmd_args = ptools_cli.parse_command_line(args)
-    cmd_args.func(cmd_args)
-
-    out, err = capfd.readouterr()
-    output_ref = AttractOutput.from_string(out)
-    output_test = AttractOutput.from_file(TEST_DOCKING_ATTRACT_OUT)
-
-    errors = output_ref.cmp(output_test)
-    if errors:
-        for e in errors:
-            print(e.message)
-    assert len(errors) == 0
+    run_attract_test(args, capfd, TEST_DOCKING_ATTRACT_OUT)
--- ptools/io.py
+++ ptools/io.py
@@ -1,10 +1,241 @@
 from __future__ import print_function
 
 import os
+import re
+import StringIO
 import sys
 
 from .forcefield import PTOOLS_FORCEFIELDS
 from .exceptions import FileParsingError
+
+
+class AttractOutputRotation(object):
+    """Class that help storing a rotation data from Attract output string."""
+
+    # Parse a minimization result line.
+    minim_re = re.compile(r'{{ minimization nb (?P<id>\d+) of (?P<total>\d+) ; '
+                          r'cutoff= (?P<cutoff>\d+\.\d+) \(A\) ; '
+                          r'maxiter= (?P<maxiter>\d+)')
+
+    def __init__(self, i=0, minimlist=[], matrix=[], energie=0.0, rmsd=0.0):
+        self.id = i
+        self.minimlist = minimlist
+        self.matrix = matrix
+        self.energie = energie
+        self.rmsd = rmsd
+
+    def number_of_minimizations(self):
+        """Return the number of minimizations ran."""
+        return len(self.minimlist)
+
+    def format_matrix(self):
+        """Return a string reprenting the matrix."""
+        s = '\n'.join(' '.join('{: 14.7f}'.format(value) for value in row)
+                      for row in self.matrix)
+        return s
+
+    @classmethod
+    def from_string(cls, content):
+        """Initialization from string."""
+        def parse_rotation_minim_output(match):
+            typemap = {'id': int, 'total': int, 'cutoff': float, 'maxiter': int}
+            minim = match.groupdict()
+            for key, value in minim.iteritems():
+                minim[key] = typemap[key](value)
+            return minim
+
+        def parse_rotation_data_line(line):
+            tokens = line.split()[1:]
+            rmsd = float('nan') if tokens[3] == 'XXXX' else float(tokens[3])
+            d = dict(trans=int(tokens[0]), rot=int(tokens[1]),
+                     ener=float(tokens[2]), rmsd=rmsd)
+            return d
+
+        minimlist = []
+        matrix = []
+        lines = content.splitlines()
+        i = int(lines[0].split()[-2])
+        for line in lines:
+            match = cls.minim_re.match(line)
+            if match:
+                minimlist.append(parse_rotation_minim_output(match))
+            elif line.startswith('=='):
+                data = parse_rotation_data_line(line)
+            elif line.startswith('MAT'):
+                matrix.append(list(float(tok) for tok in line.split()[1:]))
+        return cls(i, minimlist, matrix, data['ener'], data['rmsd'])
+
+    def assert_equal(self, other):
+        """Compare two AttractOutputRotation instances.
+
+        Raises:
+            AssertionError: if two rotations are different.
+        """
+        def isnan(num):
+            return num != num
+
+        # Compare identifiers.
+        if self.id != other.id:
+            err = 'identifiers differ: {} != {}'.format(self.id, other.id)
+            raise AssertionError(err)
+
+        # Compare matrices.
+        if self.matrix != other.matrix:
+            err = 'matrices differ:\n{}\n-----\n{}'.format(self.format_matrix(),
+                                                           other.format_matrix())
+            raise AssertionError(err)
+
+        # Compare minimizations.
+        if self.number_of_minimizations() != other.number_of_minimizations():
+            err = 'different number of minimizations: {} != {}'
+            err = err.format(self.number_of_minimizations(),
+                             other.number_of_minimizations())
+            raise AssertionError(err)
+
+        for minim1, minim2 in zip(self.minimlist, other.minimlist):
+            if minim1['cutoff'] != minim2['cutoff']:
+                err = 'minimization {}: cutoffs differ: {} != {}'
+                err = err.format(minim1['id'], minim1['cutoff'], minim2['cutoff'])
+                raise AssertionError(err)
+            if minim1['maxiter'] != minim2['maxiter']:
+                err = 'minimization {}: maxiters differ: {} != {}'
+                err = err.format(minim1['id'], minim1['maxiter'], minim2['maxiter'])
+                raise AssertionError(err)
+
+        # Compare energies.
+        if self.energie != other.energie:
+            err = 'energies differ: {} != {}'.format(self.energie, other.energie)
+            raise AssertionError(err)
+
+        # Compare RMSDs.
+        if self.rmsd != other.rmsd and not isnan(self.rmsd) and isnan(other.rmsd):
+            err = 'RMSDs differ: {} != {}'.format(self.rmsd, other.rmsd)
+            raise AssertionError(err)
+
+
+class AttractOutputTranslation(object):
+    def __init__(self, id=0, rotations=[]):
+        self.id = id
+        self.rotations = rotations
+
+    @classmethod
+    def from_string(cls, content):
+        """Initialization from string."""
+        rotations = []
+        fileobj = StringIO.StringIO(content)
+        transid = int(next(fileobj).split()[-2])
+        for lines in itercontent(fileobj, '----- Rotation nb'):
+            rot = AttractOutputRotation.from_string(lines)
+            rotations.append(rot)
+        return cls(transid, rotations)
+
+    def number_of_rotations(self):
+        """Return the number of rotations."""
+        return len(self.rotations)
+
+    def assert_equal(self, other):
+        """Compare two AttractOutputTranslation instances.
+
+        Raises:
+            AssertionError: if two translations are different.
+        """
+        if self.id != other.id:
+            err = 'translation {}: identifiers differ: {} != {}'
+            err = err.format(self.id, self.id, other.id)
+            raise AssertionError(err)
+
+        if self.number_of_rotations() != other.number_of_rotations():
+            err = 'translation {}: number of rotations differ: {} != {}'
+            err = err.format(self.id,
+                             self.number_of_translations(),
+                             other.number_of_translations())
+            raise AssertionError(err)
+
+        for rot1, rot2 in zip(self.rotations, other.rotations):
+            try:
+                rot1.assert_equal(rot2)
+            except Exception as e:
+                err = 'translation {}: rotation {}: {}'.format(self.id, rot1.id, e)
+                raise AssertionError(err)
+
+
+class AttractOutput(object):
+    def __init__(self):
+        self.translations = []
+
+    def number_of_translations(self):
+        """Return the number of translations."""
+        return len(self.translations)
+
+    def number_of_rotations(self):
+        """Return the list of the number of rotations for each translation."""
+        return [t.number_of_rotations() for t in self.translations]
+
+    @classmethod
+    def from_file(cls, path):
+        """Initialization from file."""
+        with open(path, 'rt') as stream:
+            return cls.from_stream(stream)
+
+    @classmethod
+    def from_string(cls, content):
+        """Initialization from string."""
+        return cls.from_stream(StringIO.StringIO(content))
+
+    @classmethod
+    def from_stream(cls, stream):
+        """Initialization from stream (typically file object)."""
+        output = cls()
+        for trans in itercontent(stream, '@@@@@@@ Translation nb '):
+            t = AttractOutputTranslation.from_string(trans)
+            output.translations.append(t)
+        return output
+
+    def assert_equal(self, other):
+        """Compare two AttractOutput instances.
+
+        Raises:
+            AssertionError: if two outputs are different.
+        """
+        if self.number_of_translations() != other.number_of_translations():
+            err = 'number of translations differ: {} != {}'
+            err = err.format(self.number_of_translations(),
+                             other.number_of_translations())
+            raise AssertionError(err)
+
+
+        errors = []
+        for trans1, trans2 in zip(self.translations, other.translations):
+            try:
+                trans1.assert_equal(trans2)
+            except Exception as e:
+                errors.append(e)
+        return errors
+
+
+def itercontent(fileobj, delim):
+    """Iterate over a file content.
+
+    Return the lines between two occurences of `delim`.
+
+    Args:
+        fileobj (file): iterator that allow to iterate over a file lines.
+        delim (str): separator string.
+
+    Returns:
+        iterator[str]: lines between 2 occurences of `delim`.
+    """
+    current_token = ''
+    istoken = False
+    for line in fileobj:
+        if line.startswith(delim):
+            istoken = True
+            if current_token:
+                yield current_token
+                current_token = ''
+        if istoken:
+            current_token += line
+    yield current_token
 
 
 def _print(*args, **kwargs):
@@ -91,6 +322,32 @@ def check_file_exists(path, msg="file not found: '%(path)s'", exitstatus=128,
         msg = prefix + msg % vars()
         print(msg)
         sys.exit(exitstatus)
+
+
+def read_attract_output(path):
+    """Read an Attract output file, which contains the informations that are
+    printed on stdout during a run.
+
+    Args:
+        path (str): path to file.
+
+    Returns:
+        AttractOutput: instance of class that stores attract output data.
+    """
+    return AttractOutput.from_file(path)
+
+
+def parse_attract_output(content):
+    """Parse Attract ouptut data, which contains the informations that are
+    printed on stdout during a run.
+
+    Args:
+        content (str): attract output data.
+
+    Returns:
+        AttractOutput: instance of class that stores attract output data.
+    """
+    return AttractOutput.from_string(content)
 
 
 def read_aminon(path):
