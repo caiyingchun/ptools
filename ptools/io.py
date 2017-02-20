caiyingchun/ptools

commit 0f64699e7b2921cc63bc4aa24882142b6a3d1b94

added ptools extract command
This is a minimalistic implementation of old extract.py script.
It creates no database and does not provide a --all option.

ptools/io.py
@@ -158,6 +158,30 @@ class AttractOutputTranslation(object):
                 err = 'translation {}: rotation {}: {}'.format(self.id, rot1.id, e)
                 raise AssertionError(err)
 
+    def get_rotation(self, id):
+        """Return the rotation with id `id`.
+
+        Args:
+            id (int): id of the rotation to find in the list of rotations.
+
+        Returns:
+            AttractOutputRotation: rotation with id `id`.
+        
+        Raises:
+            IndexError: if rotation id cannot be found in
+               docking ouputs.
+
+            ValueError: if multiple entries with the same id have been found.
+        """
+        rlist = [r for r in self.rotations if r.id == id]
+        if not rlist:
+            err = 'rotation identifier {} not found'.format(rotid)
+            raise IndexError(err)
+        if len(rlist) > 1:
+            err = 'multiple rotations found with id {}'.format(rotid)
+            raise ValueError(err)
+        return rlist[0]
+
 
 class AttractOutput(object):
     def __init__(self):
@@ -211,6 +235,51 @@ class AttractOutput(object):
             except Exception as e:
                 errors.append(e)
         return errors
+
+    def get_translation(self, id):
+        """Return the translation with id `id`.
+
+        Args:
+            id (int): id of the translation to find in the list of translations.
+
+        Returns:
+            AttractOutputTranslation: translation with id `id`.
+        
+        Raises:
+            IndexError: if translation id cannot be found in
+               docking ouputs.
+
+            ValueError: if multiple entries with the same id have been found.
+        """
+        tlist = [t for t in self.translations if t.id == id]
+        if not tlist:
+            err = 'translation identifier {} not found'.format(rotid)
+            raise IndexError(err)
+        if len(tlist) > 1:
+            err = 'multiple translations found with id {}'.format(rotid)
+            raise ValueError(err)
+        return tlist[0]
+
+    def get_matrix(self, transid, rotid):
+        """Return rotation matrix for docking with translation id `transid`
+        and rotation id `rotid`.
+
+        Args:
+            transid (int): docking translation identifier.
+            rotid (int): docking rotation identifier.
+
+        Return:
+            list: 4x4 transformation matrix.
+
+        Raises:
+            IndexError: if `transid` or `rotid` cannot be found in
+               docking ouputs.
+
+            ValueError: if multiple entries with the same id have been found.
+        """
+        t = self.get_translation(transid)
+        r = t.get_rotation(rotid)
+        return r.matrix
 
 
 def itercontent(fileobj, delim):

ptools/scripts/extract_cmd.py
@@ -1,0 +1,58 @@
+
+"""extract_cmd - PTools extract command."""
+
+from __future__ import print_function
+
+import sys
+
+import ptools
+
+
+def create_subparser(parent):
+    parser = parent.add_parser('extract', help=__doc__)
+    parser.set_defaults(func=run)
+    parser.add_argument('attract_output',
+                        help='attract ouptut file')
+    parser.add_argument('ligand',
+                        help='ligand reduced topology')
+    parser.add_argument('transid', type=int,
+                        help='id of the translation to extract')
+    parser.add_argument('rotid', type=int,
+                        help='id of the rotation to extract')
+    parser.add_argument('-o', '--output',
+                        help='output topology file name (by default print '
+                             'on stout)')
+
+
+def rigidXMat44(rigid, mat):
+    out = ptools.Rigidbody(rigid)
+    for i in range(len(rigid)):
+        coords = rigid.getCoords(i)
+        coords2 = ptools.Coord3D()
+        coords2.x = mat[0][0] * coords.x + mat[0][1] * coords.y + mat[0][2] * coords.z + mat[0][3]
+        coords2.y = mat[1][0] * coords.x + mat[1][1] * coords.y + mat[1][2] * coords.z + mat[1][3]
+        coords2.z = mat[2][0] * coords.x + mat[2][1] * coords.y + mat[2][2] * coords.z + mat[2][3]
+        out.setCoords(i, coords2)
+    return out
+
+
+def run(args):
+    ptools.io.check_file_exists(args.attract_output)
+    ptools.io.check_file_exists(args.ligand)
+
+    # Get transformation matrix from attract output file.
+    docking_result = ptools.io.read_attract_output(args.attract_output)
+    m = docking_result.get_matrix(args.transid, args.rotid)
+
+    # Read ligand topology and apply transformation.
+    ligand = ptools.Rigidbody(args.ligand)
+    ligand_out = rigidXMat44(ligand, m)
+
+    # Print output topology.
+    header = 'REMARK 999 TRANSLATION ROTATION  {} {}'.format(args.transid,
+                                                             args.rotid)
+    if args.output:
+        with open(args.output, 'wt') as f:
+            print(header, ligand_out, sep='\n', file=f)
+    else:
+        print(header, ligand_out, sep='\n', file=sys.stdout)

ptools/scripts/ptools_cli.py
@@ -9,6 +9,7 @@ import argparse
 import ptools
 
 from . import attract_cmd
+from . import extract_cmd
 from . import reduce_cmd
 from . import translate_cmd
 
@@ -20,6 +21,7 @@ def parse_command_line(args=None):
     attract_cmd.create_subparser(subparsers)
     reduce_cmd.create_subparser(subparsers)
     translate_cmd.create_subparser(subparsers)
+    extract_cmd.create_subparser(subparsers)
     return parser.parse_args(args)
 
 

ptools/scripts/reduce_cmd.py
@@ -1,5 +1,5 @@
 
-"""PTools reduce command."""
+"""reduce_cmd - PTools reduce command."""
 
 from __future__ import print_function
 

commit 1e5b4c4aecac5f684583110604dd22ef1bd07d66

corrected bugs introduced in 0f64699

ptools/io.py
@@ -166,7 +166,7 @@ class AttractOutputTranslation(object):
 
         Returns:
             AttractOutputRotation: rotation with id `id`.
-        
+
         Raises:
             IndexError: if rotation id cannot be found in
                docking ouputs.
@@ -175,10 +175,10 @@ class AttractOutputTranslation(object):
         """
         rlist = [r for r in self.rotations if r.id == id]
         if not rlist:
-            err = 'rotation identifier {} not found'.format(rotid)
+            err = 'rotation identifier {} not found'.format(id)
             raise IndexError(err)
         if len(rlist) > 1:
-            err = 'multiple rotations found with id {}'.format(rotid)
+            err = 'multiple rotations found with id {}'.format(id)
             raise ValueError(err)
         return rlist[0]
 
@@ -227,7 +227,6 @@ class AttractOutput(object):
                              other.number_of_translations())
             raise AssertionError(err)
 
-
         errors = []
         for trans1, trans2 in zip(self.translations, other.translations):
             try:
@@ -244,7 +243,7 @@ class AttractOutput(object):
 
         Returns:
             AttractOutputTranslation: translation with id `id`.
-        
+
         Raises:
             IndexError: if translation id cannot be found in
                docking ouputs.
@@ -253,10 +252,10 @@ class AttractOutput(object):
         """
         tlist = [t for t in self.translations if t.id == id]
         if not tlist:
-            err = 'translation identifier {} not found'.format(rotid)
+            err = 'translation identifier {} not found'.format(id)
             raise IndexError(err)
         if len(tlist) > 1:
-            err = 'multiple translations found with id {}'.format(rotid)
+            err = 'multiple translations found with id {}'.format(id)
             raise ValueError(err)
         return tlist[0]
 