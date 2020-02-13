4b6feb6e94e2f97186173df62d0800a5d11381ac
Benoist LAURENT <benoist.laurent@gmail.com>
2020-02-13 11:29:31 +0100

    bugfix: create output directory if does not exist

--- ptools/io.py
+++ ptools/io.py
@@ -563,7 +563,6 @@ class DockingOutputList(list):
         if not os.path.exists(output_directory):    
             msg = "creating output_directory '{}'".format(output_directory)
             print("{}: {}".format(me, msg), file=sys.stderr)
-            os.mkdir(output_directory)
 
         msg = "writing clusters to output directory '{}'".format(output_directory)
         print("{}: {}".format(me, msg), file=sys.stderr)
